82c7a03041be3826669bd7e5e60211c4d263325c
nkocheshkov <kocheschkov@sel-be.ru>
2024-06-17 12:55:04 +0300

    add dist

---
+++ src/main/java/ru/cynteka/Services/JsonElement.java
@@ -0,0 +1,12 @@
+package ru.cynteka.Services;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.PARAMETER)
+public @interface JsonElement {
+
+}
